8b83493e257ec3ac7a1802410fdc9071908b3349
anandakrishnankb <anandakrishnankb001@gmail.com>
2024-04-15 21:20:09 +0530

    react-home-pg

---
+++ django_lms/frontend/src/components/Home.js
@@ -0,0 +1,167 @@
+function Home() {
+  return (
+    <div className="container mt-4">
+      {/* Latest Courses*/}
+      <h3 className=" pb-1 mb-4">Latest Courses <a href="#" className="float-end">See All</a></h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Course title</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Course title</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Course title</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Course title</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Latest Courses*/}
+      {/* Popular Courses*/}
+      <h3 className=" pb-1 mb-4 mt-5">Popular Courses<a href="#" className="float-end">See All</a></h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Course title</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Course title</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Course title</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Course title</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Popular Courses*/}
+      {/* Popular Teachers*/}
+      <h3 className=" pb-1 mb-4 mt-5">Popular Teachers<a href="#" className="float-end">See All</a></h3>
+      <div className="row">
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Teacher Name</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Teacher Name</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Teacher Name</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+        <div className="col-md-3">
+          <div className="card">
+            <a href="#">
+              <img src="logo512.png" className="card-img-top" alt="..." />
+            </a>
+            <div className="card-body">
+              <h5 className="card-title">
+                <a href="#"> Teacher Name</a>
+              </h5>
+            </div>
+          </div>
+        </div>
+      </div>
+      {/* End Popular Teachers*/}
+    </div>
+  );
+}
+
+export default Home;

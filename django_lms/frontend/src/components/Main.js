55474e3cb46724a38012624e17196bb937bf10d8
anandakrishnankb <anandakrishnankb001@gmail.com>
2024-04-15 21:40:11 +0530

    footer

--- django_lms/frontend/src/components/Main.js
+++ django_lms/frontend/src/components/Main.js
@@ -1,11 +1,13 @@
 import Home from "./Home";
 import Header from "./Header";
+import Footer from "./Footer"
 
 function Main() {
   return (
     <div className="App">
       <Header />
       <Home />
+      <Footer/>
     </div>
   );
 }
